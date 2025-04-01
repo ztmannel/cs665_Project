6b136c2a36af69bfcc4b342f07f73b4033bafa2f
Zach <ztmannel@shockers.wichita.edu>
2025-03-31 21:58:17 -0500

    completed create.sql, crud.sql, and insert.sql

--- create.sql
+++ create.sql
@@ -1,59 +1,59 @@
-/* 
-employee_personal_info(first_name, last_name, position, phone, address, city, state, country, employee_id(PK), personal_email);
-badge_info(badge id(FK), employee_id (PK),deactivation_date,activation_date);
-compensation_table (employee_id (PK), salary, bonus, timestamp);
-badge_sign_in_times (badge id(PK), date scanned, time scanned);
-employee_company_info(employee_id (PK), company email, department, manager_emp_id, hire date, termination date);
-employee_time_off (employee_id (PK), hours remaining, hours taken, total annual hours); 
-*/
-
 CREATE TABLE IF NOT EXISTS employee_personal_info(
-    first_name      TEXT    NOT NULL, 
-    last_name       TEXT    NOT NULL,
-    position        TEXT    NOT NULL,
-    phone           INTEGER CHECK (phone GLOB '???-???-????')   NOT NULL,
-    address         TEXT    NOT NULL,
-    city            TEXT    NOT NULL,
-    state           TEXT    NOT NULL,
-    country         TEXT    NOT NULL,
-    personal_email  TEXT    NOT NULL,
-    PRIMARY KEY (employee_id) REFERENCES (badge_info, compensation_table, employee_company_info, employee_time_off)(employee_id) ON DELETE CASCADE
+    employee_id     INTEGER,
+    first_name      TEXT            NOT NULL, 
+    last_name       TEXT            NOT NULL,
+    position        TEXT            NOT NULL,
+    phone           INTEGER         CHECK (phone GLOB '???-???-????')   NOT NULL,
+    address         TEXT            NOT NULL,
+    city            TEXT            NOT NULL,
+    state           TEXT            NOT NULL,
+    country         TEXT            NOT NULL,
+    personal_email  TEXT    UNIQUE  NOT NULL,
+    PRIMARY KEY (employee_id) --REFERENCES badge_info, compensation_table, employee_company_info, employee_time_off(employee_id) ON DELETE CASCADE
 );
 
-/*date is in DDMMYYYY format*/
+--date is in DDMMYYYY format; employee_id relates to the employee_personal_info.employee_id (functional dependency)
 CREATE TABLE IF NOT EXISTS badge_info(
+    badge_id            INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    employee_id         INTEGER NOT NULL,
     activation_date     INTEGER NOT NULL,
     deactivation_date   INTEGER NOT NULL,
-    FOREIGN KEY(employee_id),
-    PRIMARY KEY AUTOINCREMENT (badge_id) REFERENCES (badge_sign_in_times)(badge_id) ON DELETE CASCADE
+    FOREIGN KEY(employee_id) REFERENCES employee_personal_info(employee_id)
 );
 
+--employee_id relates to the employee_personal_info.employee_id
 CREATE TABLE IF NOT EXISTS compensation_table(
-    salary              INTEGER NOT NULL,
+    employee_id			INTEGER	NOT NULL,
+	salary              INTEGER NOT NULL,
     bonus               INTEGER NOT NULL,
     salary_set_date     INTEGER NOT NULL,
-    FOREIGN KEY(employee_id)
+    FOREIGN KEY(employee_id) REFERENCES employee_personal_info(employee_id)
 );
 
-/*make ref to the badge info table. Date is in DDMMYYYY. time is in HHmm format*/
+/*Date is in DDMMYYYY. time is in HHmm format. badge_id relates to the badge_info.badge_id*/
 CREATE TABLE IF NOT EXISTS badge_sign_in_times(
-    date_scanned    INTEGER,
+    badge_id		INTEGER	NOT NULL,
+	date_scanned    INTEGER,
     time_scanned    INTEGER,
-    FOREIGN KEY (badge id)
+    FOREIGN KEY (badge_id) REFERENCES badge_info(badge_id)
 );
 
+--employee_id relates to the employee_personal_info.employee_id
 CREATE TABLE IF NOT EXISTS employee_company_info(
-    company_email       TEXT    NOT NULL,
-    department          TEXT    NOT NULL,
-    manager_emp_id      INTEGER NOT NULL,
-    hire_date           INTEGER NOT NULL,
+    employee_id			INTEGER	        NOT NULL,
+	company_email       TEXT    UNIQUE  NOT NULL,
+    department          TEXT            NOT NULL,
+    manager_emp_id      INTEGER         NOT NULL,
+    hire_date           INTEGER         NOT NULL,
     termination_date    INTEGER,
-    FOREIGN KEY (employee_id)
+    FOREIGN KEY (employee_id) REFERENCES employee_personal_info(employee_id)
 );
 
+--employee_id relates to the employee_personal_info.employee_id
 CREATE TABLE IF NOT EXISTS employee_time_off(
-    hours_remaining     INTEGER,
+    employee_id			INTEGER	NOT NULL,
+	hours_remaining     INTEGER,
     hours_consumed      INTEGER,
     total_annual_hours  INTEGER,
-    FOREIGN KEY(employee_id)
+    FOREIGN KEY(employee_id) REFERENCES employee_personal_info(employee_id)
 );
